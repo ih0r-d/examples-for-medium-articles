50465a63a8281d14eac688c89f0c30f8ecc5d229
Ihor Didyk <ihordidyk95@gmail.com>
2024-12-26 17:15:51 +0200

    init project

---
+++ src/main/java/com/ih0rd/sandbox/MediumArticlesK8sGenerationSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.ih0rd.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediumArticlesK8sGenerationSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediumArticlesK8sGenerationSandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ih0rd/sandbox/MediumArticlesK8sGenerationSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ih0rd.sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediumArticlesK8sGenerationSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
